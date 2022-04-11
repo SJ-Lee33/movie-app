7febc921b41283dca8c2c997378779c8c7d13b95
SJ-Lee33 <isjift55@ewhain.net>
2022-04-11 11:43:57 +0900

    spa

--- src/App.js
+++ src/App.js
@@ -26,29 +26,31 @@ class App extends Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section>
-        {isLoading ? (
-          <div>
-            <span>Loading...</span>
-          </div>
-        ) : (
-          <div>
-            {movies.map((data) => {
-              return (
-                <Movie
-                  key={data.id}
-                  id={data.id}
-                  title={data.title}
-                  year={data.year}
-                  genres={data.genres}
-                  summary={data.summary}
-                  cover_img={data.medium_cover_image}
-                />
-              );
-            })}
-          </div>
-        )}
-      </section>
+      <Router basename="./movie-app">
+        <section>
+          {isLoading ? (
+            <div>
+              <span>Loading...</span>
+            </div>
+          ) : (
+            <div>
+              {movies.map((data) => {
+                return (
+                  <Movie
+                    key={data.id}
+                    id={data.id}
+                    title={data.title}
+                    year={data.year}
+                    genres={data.genres}
+                    summary={data.summary}
+                    cover_img={data.medium_cover_image}
+                  />
+                );
+              })}
+            </div>
+          )}
+        </section>
+      </Router>
     );
   }
 }
